357aa4086535a3e6488cc6a1445cce0a48fc465c
jieatcoderrect <jie@coderrect.com>
2020-07-01 19:10:01 -0700

    initial checkin of race.cpp

---
+++ race.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <pthread.h>
+
+
+int x;
+
+
+void* worker(void* unused) {
+    x = rand();
+    std::cout << x << "\n";
+    return nullptr;
+}
+
+
+int main() {
+    pthread_t th1, th2;
+
+    pthread_create(&th1, nullptr, worker, nullptr);
+    pthread_create(&th2, nullptr, worker, nullptr);
+
+    pthread_join(th1, nullptr);
+    pthread_join(th2, nullptr);
+
+    return 0;
+}
+
